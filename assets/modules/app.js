91f9f17ecc3d5b6bfae469bf011c69e19b735b22
wangxian <xian366@126.com>
2013-04-20 22:26:49 +0800

    新加了新功能后，最新完成度，90%，剩下资源图。

--- assets/modules/app.js
+++ assets/modules/app.js
@@ -6,10 +6,15 @@ define(function(require) {
     el: 'body',
     events: {
       'click .container img': "btnImg",
-      'click .ui-dialog-close': "btnClose"
+      'click .ui-dialog-close': "btnClose",
+      'mouseenter .box-product li': 'mouseEnter',
+      'mouseleave .box-product li': 'mouseLeave',
+      'click .box-product li>img' : 'btnShow',
+      'click #img-video .close'   :  "btnProductClose"
     },
     initialize: function(){
-
+      this.offset = $(".container").offset();
+      // alert( this.offset.left +','+ this.offset.top);
     },
     btnImg: function(e){
       var _e = this.$(e.currentTarget);
@@ -21,29 +26,57 @@ define(function(require) {
         return false;
       }
 
-      var style = _e.position();
-      // style.left = e.clientX;
-      // style.top  = e.clientY;
-      // console.log(_e.position());
-
-      var offset = _e.attr('data-offset');
+      var _style  = $(".container").offset();
+      var _offset = _e.attr('data-offset');
       var offsetAP;
-      if(offset) {
-        offsetAP = offset.split(',');
-        style.left = style.left - offsetAP[0];
-        style.top  = style.top - offsetAP[1];
+      if(_offset) {
+        offsetAP = _offset.split(',');
+        _style.left = _style.left + offsetAP[0]*1;
+        _style.top  = _style.top  + offsetAP[1]*1;
       }
-      // console.log(style);
+      console.log(this.offset, _style);
 
       it.$el.find('#mask').fadeIn(300, function(){
-        it.$el.find('.ui-dialog').filter('.'+ className).css(style).addClass('on');
+        it.$el.find('.ui-dialog').filter('.'+ className).css(_style).addClass('on');
       });
     },
+
     btnClose: function(e){
       var it = this;
       it.$el.find('#mask').fadeOut();
       it.$el.find('.ui-dialog.on').removeClass('on');
+    },
+
+    btnProductClose: function(e){
+      var it = this;
+      it.$el.find('#mask-dialog').fadeOut();
+      it.$el.find('#img-video .content').empty();
+      it.$el.find('#img-video').hide();
+    },
+
+    mouseEnter: function(e) {
+      var _e = $(e.currentTarget);
+      _e.find('.tip').addClass('off');
+      // console.log('enter');
+    },
+
+    mouseLeave: function(e) {
+      var _e = $(e.currentTarget);
+      _e.find('.tip').removeClass('off');
+      // console.log('leave');
+    },
+
+    btnShow: function(e) {
+      var _e = $(e.currentTarget);
+      var _type = _e.data('type');
+
+      $("#img-video .content").html('<iframe height=498 width=510 src="http://player.youku.com/embed/XNTQ1MTIyMTc2" frameborder=0 allowfullscreen></iframe>');
+      $('#mask-dialog').show();
+      $('#img-video').show();
     }
+
+
+
 
   });
 
